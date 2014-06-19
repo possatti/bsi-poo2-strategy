cd06ec0283966d599ea1b6f36543919f8f41d4ba
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-06-19 15:26:39 -0300

    Implementação do main
    	modificado: main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,38 @@
 #include <cstdlib>
+#include <iostream>
+
+#include "Context.h"
+#include "Add.h"
+#include "Subtract.h"
+#include "Multiply.h"
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv)
 {
+
+    Context* context;
+    int a = 3;
+    int b = 2;
+
+    context = new Context(new Add());
+    int resultA = context->executeStrategy(a, b);
+
+    context->~Context();
+
+    context = new Context(new Subtract());
+    int resultB = context->executeStrategy(a, b);
+
+    context->~Context();
+
+    context = new Context(new Multiply());
+    int resultC = context->executeStrategy(a, b);
+
+    context->~Context();
+
+    cout << "Result A: " << resultA << endl;
+    cout << "Result B: " << resultB << endl;
+    cout << "Result C: " << resultC << endl;
 
     return 0;
 }
